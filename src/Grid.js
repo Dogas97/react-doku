7db2543d4ea78249a4f9a4c5bf6f5802b9589fba
Dogas <diogofilipedias5@gmail.com>
2022-08-10 20:25:16 +0100

    Created a simple Matrix Layout

--- src/Grid.js
+++ src/Grid.js
@@ -1,13 +1,30 @@
 export default function Grid({ grid }) {
     return (
         <>
-            {
-                grid.values && grid.values.map((valuesX) =>
-                    valuesX.map((value, id) =>
-                        value ? <p key={id}>{value}</p> : <input key={id} type="text" />
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <div>State: {grid.state}</div>
+                <div>size: {grid.size}</div>
+                <div>dificulty: {grid.dificulty}</div>
+
+                {
+                    grid.values && grid.values.map((valuesX, id) =>
+                        <div key={"line" + id} style={{ display: "flex" }}>
+                            {
+                                valuesX.map((value, id) =>
+                                    <div
+                                        key={"column" + id}
+                                        style={{ width: "25px", textAlign: "center" }}>
+                                        {
+                                            value ? value
+                                                : <input key={"column" + id} style={{ width: "100%", padding: 0 }} />
+                                        }
+                                    </div>
+                                )
+                            }
+                        </div>
                     )
-                )
-            }
+                }
+            </div>
         </>
     )
 }
