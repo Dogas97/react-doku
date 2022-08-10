ffd46ccea782c256b809056e0c0aabd9eb933e9d
Dogas <diogofilipedias5@gmail.com>
2022-08-10 21:03:32 +0100

    Better Grid Layout and maxLength in cells

--- src/Grid.js
+++ src/Grid.js
@@ -1,29 +1,60 @@
 export default function Grid({ grid }) {
     return (
         <>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                <div>State: {grid.state}</div>
-                <div>size: {grid.size}</div>
-                <div>dificulty: {grid.dificulty}</div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}>
+                <div style={{
+                    margin: 15,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                }}>
+                    <div><b>State:</b> {grid.state}</div>
+                    <div><b>Size:</b> {grid.size}</div>
+                    <div><b>Dificulty:</b> {grid.dificulty}</div>
+                </div>
 
-                {
-                    grid.values && grid.values.map((valuesX, id) =>
-                        <div key={"line" + id} style={{ display: "flex" }}>
-                            {
-                                valuesX.map((value, id) =>
-                                    <div
-                                        key={"column" + id}
-                                        style={{ width: "25px", textAlign: "center" }}>
-                                        {
-                                            value ? value
-                                                : <input key={"column" + id} style={{ width: "100%", padding: 0 }} />
-                                        }
-                                    </div>
-                                )
-                            }
-                        </div>
-                    )
-                }
+                <div style={{
+                    border: "4px solid black"
+                }}>
+                    {
+                        grid.values && grid.values.map((valuesX, id) =>
+                            <div key={"line" + id} style={{ display: "flex" }}>
+                                {
+                                    valuesX.map((value, id) =>
+                                        <div
+                                            key={"column" + id}
+                                            style={{
+                                                width: 25,
+                                                height: 25,
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                                border: "1px solid black"
+                                            }}>
+                                            {
+                                                value ? value
+                                                    : <input
+                                                        key={"column" + id}
+                                                        maxLength={Math.sqrt(grid.size).toString().length}
+                                                        style={{
+                                                            border: 0,
+                                                            padding: 0,
+                                                            width: "100%",
+                                                            height: "100%",
+                                                            textAlign: "center"
+                                                        }} />
+                                            }
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </>
     )
