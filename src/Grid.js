5a3be07bda6eeedeadcf600a16ebe3fd597f643d
Dogas97 <diogofilipedias5@gmail.com>
2022-08-12 13:30:57 +0100

    Minor fixes in the Grid & changed texts for State / Difficulty

--- src/Grid.js
+++ src/Grid.js
@@ -1,67 +1,75 @@
 export default function Grid({ grid }) {
     return (
         <>
-            <div style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-            }}>
-                <div style={{
-                    marginTop: 15,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center"
-                }}>
-                    <div><b>State:</b> {grid.state}</div>
-                    <div><b>Size:</b> {grid.size} x {grid.size}</div>
-                    <div><b>Difficulty:</b> {grid.difficulty}</div>
-                </div>
-
-                {
-                    grid.state === 1 ? 
+            {
+                grid ?
+                    <div style={{
+                        margin: "15px 0",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}>
                         <div style={{
-                            margin: "15px 0",
-                            border: "4px solid black"
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center"
                         }}>
-                            {
-                                grid.values.map((valuesX, id) =>
-                                    <div key={"line" + id} style={{ display: "flex" }}>
-                                        {
-                                            valuesX.map((value, id) =>
-                                                <div
-                                                    key={"column" + id}
-                                                    style={{
-                                                        width: 25,
-                                                        height: 25,
-                                                        display: "flex",
-                                                        alignItems: "center",
-                                                        justifyContent: "center",
-                                                        border: "1px solid black"
-                                                    }}>
-                                                    {
-                                                        value ? value
-                                                            : <input
-                                                                key={"column" + id}
-                                                                maxLength={grid.size.toString().length}
-                                                                style={{
-                                                                    border: 0,
-                                                                    padding: 0,
-                                                                    width: "100%",
-                                                                    height: "100%",
-                                                                    textAlign: "center"
-                                                                }} />
-                                                    }
-                                                </div>
-                                            )
-                                        }
-                                    </div>
-                                )
-                            }
+                            <div><b>State:</b> {grid.state === 0 ? "Ready" : grid.state === 1  ? "Started" : "Finished"}</div>
+                            <div><b>Size:</b> {grid.size} x {grid.size}</div>
+                            <div><b>Difficulty:</b> {grid.difficulty === 1 ? "Easy" :grid.difficulty === 2 ? "Normal": "Hard"}</div>
                         </div>
-                    : 
-                        <></>
-                }
-            </div>
+
+                        {
+                            grid.state === 1 ? 
+                                <div style={{
+                                    marginTop: 15,
+                                    border: "4px solid black"
+                                }}>
+                                    {
+                                        grid.values.map((valuesX, id) =>
+                                            <div key={"line" + id} style={{ display: "flex" }}>
+                                                {
+                                                    valuesX.map((value, id) =>
+                                                        <div
+                                                            key={"column" + id}
+                                                            style={{
+                                                                width: 25,
+                                                                height: 25,
+                                                                display: "flex",
+                                                                alignItems: "center",
+                                                                justifyContent: "center",
+                                                                border: "1px solid black"
+                                                            }}>
+                                                            {
+                                                                value ?
+                                                                    <div style={{fontWeight: "bold"}}>{value}</div>
+                                                                : 
+                                                                    <input
+                                                                        key={"column" + id}
+                                                                        maxLength={grid.size.toString().length}
+                                                                        style={{
+                                                                            border: 0,
+                                                                            padding: 0,
+                                                                            width: "100%",
+                                                                            height: "100%",
+                                                                            textAlign: "center"
+                                                                        }} 
+                                                                    />
+                                                            }
+                                                        </div>
+                                                    )
+                                                }
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            : 
+                                <></>
+                        }
+                    </div>
+                :
+                    <></>
+            }
         </>
     )
 }
